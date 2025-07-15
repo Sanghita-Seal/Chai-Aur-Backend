9825de21f21feadf2fa11c4f839359eac454ba0e
Sanghita Seal <sealsanghita2005@gmail.com>
2025-07-15 09:55:37 +0530

    Remove unused import of verify from jsonwebtoken in user routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -13,7 +13,6 @@ import { changeCurrentPassword,
 } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { verify } from "jsonwebtoken";
 
 const router= Router()
 
