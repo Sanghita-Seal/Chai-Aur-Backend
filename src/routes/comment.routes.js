d9d799600f2e37bd9cafc7e315c2790a0897e885
Sanghita Seal <sealsanghita2005@gmail.com>
2025-07-16 21:04:13 +0530

    Basic structure of all the routes added

---
+++ src/routes/comment.routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+
+import {
+    createComment,
+    getComments,
+    updateComment,  
+    deleteComment,
+} from "../controllers/comment.controller.js";  
+
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router();
+
+router.use(verifyJWT); //Apply verifyJWT middleware to all the routes in this file
+
+router.route("/:videoId").get(getVideoComments).post(updateComment)
+router.route("c/:commentId").delete(deleteComment).patch(updateComment)
+
+export default router;
