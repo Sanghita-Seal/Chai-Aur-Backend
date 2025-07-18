fece78dff2cd0eb91a43ea112b6c6626f3bac459
Sanghita Seal <sealsanghita2005@gmail.com>
2025-07-18 21:00:56 +0530

    Add simulated error handling in healthcheck controller for testing purposes

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -6,6 +6,8 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
     res.status(200).json(new ApiResponse("OK", "Service is healthy").toJSON())
+    // If you want to throw an error for testing purposes, uncomment the line below
+    throw new ApiError(500, "Simulated error for testing purposes")      
 
     
 })
